675020a008cefb3d7c8fcc8e92a5ed6c76627b3e
anaclwra <anaclaraschlickmann97@gmail.com>
2021-12-08 17:30:27 -0300

    firts commit'

---
+++ componentes/concluiTarefa.js
@@ -0,0 +1,22 @@
+const concluirTarefa = (atualiza, id) => {
+    const tarefaCadastradas= JSON.parse(localStorage.getItem("tarefas"))
+ 
+    tarefaCadastradas[id].concluida= !tarefaCadastradas[id].concluida
+ 
+    localStorage.setItem("tarefas", JSON.stringify(tarefaCadastradas))
+ 
+    atualiza()
+ }
+
+export const BotaoConclui = (atualiza, id) => {
+    const botaoConclui = document.createElement("button")
+
+    botaoConclui.classList.add("check-button")
+    botaoConclui.innerText = "Concluir"
+    botaoConclui.addEventListener("click", () => concluirTarefa(atualiza, id))
+
+    return botaoConclui
+}
+
+//export default botaoConclui
+
